bfc9bffb2aef2efac50f9a1e56f377d29912030e
Orkhan Maharramli <orkhan@Orkhans-MacBook-Air.local>
2017-06-11 17:32:04 +0400

    Get Results

--- lib/goldenpay/request.rb
+++ lib/goldenpay/request.rb
@@ -5,6 +5,36 @@ require 'json'
 
 module Goldenpay
   module Request
+    def self.get_results(payment_key)
+
+      auth_key = Goldenpay.auth_key
+      hash_code = Digest::MD5.hexdigest(auth_key + payment_key)
+
+      uri = URI(Goldenpay.payment_result_url + '?payment_key=' + payment_key + '&hash_code=' + hash_code)
+
+      # Create client
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+      # Create Request
+      req =  Net::HTTP::Post.new(uri)
+      req.add_field "Content-Type", "application/json"
+      req.add_field "Accept", "application/json"
+
+      # Fetch Request
+      res = http.request(req)
+
+      parsed_res = JSON.parse(res.body)
+
+      if parsed_res["status"]["code"] == 1
+        parsed_res
+      else
+        raise GoldenpayResponseError, "Goldenpay responded with: #{parsed_res["status"]["code"]} | #{parsed_res["status"]["message"]}"
+      end
+    rescue StandardError => e
+      raise HTTPResponseError, "Goldenpay request failed: #{res.code}"
+    end
 
     def self.get_payment_key(options)
 
